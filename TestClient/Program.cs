dd1815287c21dc4aa1a5a2c855cfdd6d3ba30ea4
Владислав Звержховский <wladzwer@mail.ru>
2021-03-22 18:58:33 +0300

    temp

---
+++ TestClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using Grpc.Net.Client;
+using System;
+using System.Net.Http;
+
+namespace TestClient
+{
+    class Program
+    {
+        private static GrpcChannel Channel;
+        private static OrderBoard.OrderBoardClient Client;
+        static void Main(string[] args)
+        {
+            AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            var httpHandler = new HttpClientHandler();
+            // Return `true` to allow certificates that are untrusted/invalid
+            httpHandler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            Channel = GrpcChannel.ForAddress("http://176.119.156.220:5005", new GrpcChannelOptions { HttpHandler = httpHandler });
+            Client = new OrderBoard.OrderBoardClient(Channel);
+            var result = Client.GetOrder(new Google.Protobuf.WellKnownTypes.Empty());
+        }
+    }
+}
