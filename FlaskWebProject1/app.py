bffe9ffb8976e13f314ced6320246f8334750e12
vladzvx <wladzwer@mail.ru>
2021-04-02 23:37:19 +0300

    test flask application upd

--- FlaskWebProject1/app.py
+++ FlaskWebProject1/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def hello():
     """Renders a sample page."""
-    return "Hello World!"
+    return os.environ.get('grpc_host')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
