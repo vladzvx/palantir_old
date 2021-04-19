00b0de9bcc0ed0298bf41ba686ac6a520b760295
Владислав Звержховский <wladzwer@mail.ru>
2021-04-19 23:15:29 +0300

    orders lifecycle update

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -12,12 +12,12 @@ namespace TestClient
         {
             AppContext.SetSwitch(
     "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            GrpcChannel Channel = GrpcChannel.ForAddress("http://45.132.17.172:5005");
+            GrpcChannel Channel = GrpcChannel.ForAddress("http://:5005");
             OrderBoard.OrderBoardClient  Client = new OrderBoard.OrderBoardClient(Channel);
             while (true)
             {
                 var order = Client.GetOrder(new Google.Protobuf.WellKnownTypes.Empty());
-                Console.WriteLine(string.Format("{0};{1}",order.Type,order.RedirectCounter));
+                Console.WriteLine(string.Format("{0};{1};{2}", order.Link,order.Type,order.RedirectCounter));
                 //if(order.Type==OrderType.History)
                     Client.PostOrder(order);
                 Thread.Sleep(100);
