c305b9fe772edf33af7d9d1cd7d2953131145040
Владислав Звержховский <wladzwer@mail.ru>
2021-06-08 15:17:57 +0300

    python code gen from .proto update

--- DataLoader/generateConfigurator.bat
+++ DataLoader/generateConfigurator.bat
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -I../Common --python_out=. --grpc_python_out=. ../Common/Configurator.proto
+python -m grpc_tools.protoc -I../Common/Proto --python_out=. --grpc_python_out=. ../Common/Proto/Configurator.proto
--- DataLoader/generateOrderBoard.bat
+++ DataLoader/generateOrderBoard.bat
@@ -1,1 +1,1 @@
-python -m grpc_tools.protoc -I../Common --python_out=. --grpc_python_out=. ../Common/OrderBoard.proto
+python -m grpc_tools.protoc -I../Common/Proto --python_out=. --grpc_python_out=. ../Common/Proto/OrderBoard.proto
