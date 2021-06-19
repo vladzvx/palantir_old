762a2e4028c76673a470a1213a28937052b214bf
Владислав Звержховский <wladzwer@mail.ru>
2021-06-20 01:01:19 +0300

    bugs fixes

--- Common/Services/DataBase/ConnectionPoolManager.cs
+++ Common/Services/DataBase/ConnectionPoolManager.cs
@@ -25,15 +25,15 @@ namespace Common.Services.DataBase
             timer.AutoReset = true;
             timer.Start();
 
-            ConnectionWrapper connection = new ConnectionWrapper(settings.ConnectionString, this);
-            connection.Connection.Open();
-            PoolRepo.TryAdd(connection.Id, connection);
-            Pool.Add(connection);
+            //ConnectionWrapper connection = new ConnectionWrapper(settings.ConnectionString, this);
+            //connection.Connection.Open();
+            //PoolRepo.TryAdd(connection.Id, connection);
+            //Pool.Add(connection);
 
-            ConnectionWrapper connection2 = new ConnectionWrapper(settings.ConnectionString, this);
-            connection2.Connection.Open();
-            PoolRepo.TryAdd(connection2.Id, connection2);
-            Pool.Add(connection2);
+            //ConnectionWrapper connection2 = new ConnectionWrapper(settings.ConnectionString, this);
+            //connection2.Connection.Open();
+            //PoolRepo.TryAdd(connection2.Id, connection2);
+            //Pool.Add(connection2);
         }
 
         private void TimerAction(object sender, System.Timers.ElapsedEventArgs args)
