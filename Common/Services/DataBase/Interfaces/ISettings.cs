4916cecbee4052cd813077c9285a296ce08da673
Владислав Звержховский <wladzwer@mail.ru>
2021-08-16 22:59:22 +0300

    hardcode fix

--- Common/Services/DataBase/Interfaces/ISettings.cs
+++ Common/Services/DataBase/Interfaces/ISettings.cs
@@ -8,6 +8,6 @@ namespace Common.Services.DataBase.Interfaces
 {
     public interface ISettings
     {
-        public int OrdersManagerCheckingPeriod => 1 * 60 * 1000;
+        public int OrdersManagerCheckingPeriod => 3 * 60 * 1000;
     }
 }
