f642d932b944387dfa1488391efa00e55f36b5d3
Владислав Звержховский <wladzwer@mail.ru>
2021-10-29 08:47:37 +0300

    add docker compose

--- NotificationProvider/Program.cs
+++ NotificationProvider/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
@@ -14,24 +16,6 @@ namespace NotificationProvider
     {
         public static void Main(string[] args)
         {
-
-            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-            List<Task> tsks = new List<Task>();
-            for (int i = 0; i < 10; i++)
-            {
-                tsks.Add(Task.Factory.StartNew(() =>
-                {
-                    for (int i2 = 0; i2 < 10; i2++)
-                    {
-                        byte[] rndm = new byte[1];
-                        rng.GetBytes(rndm);
-                        Console.WriteLine(rndm[0]);
-
-                    }
-                }));
-            }
-            Task.WhenAll(tsks).Wait();
-
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -39,6 +23,13 @@ namespace NotificationProvider
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.Listen(IPAddress.Any, 5000, o =>
+                        {
+                            o.Protocols = HttpProtocols.Http1;
+                        });
+                    });
                     webBuilder.UseStartup<Startup>();
                 });
     }
