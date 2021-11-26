c8f814ed5765a2913e2954daf6146b19df3453ed
Vlad Zv <71317042+vladzvx@users.noreply.github.com>
2021-11-26 22:57:47 +0300

    Delete test.sql

--- test.sql
+++
@@ -1,4 +0,0 @@
-create table test (test_field text);
-insert into test (test_field) values ('sdddd');
-insert into test (test_field) values ('sdddd');
-insert into test (test_field) values ('sdddd');
